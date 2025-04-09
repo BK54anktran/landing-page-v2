48252c4f47fd5260ff60c014244bb7a1734a9cb7
pigeatgarlic <huyhoangdo0205@gmail.com>
2025-04-09 04:49:23 +0000

    fix logo

--- components/providers/stateProvider.tsx
+++ components/providers/stateProvider.tsx
@@ -22,7 +22,7 @@ export const Header = ({ openLogin }: { openLogin?: () => void }) => {
         <header>
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <a href="/play/" className="flex items-center">
+                    <a href="/play/index.html" className="flex items-center">
                         <img
                             src="/img/logo_white.png"
                             className="mr-3 h-6 sm:h-9"
@@ -43,7 +43,7 @@ export const Header = ({ openLogin }: { openLogin?: () => void }) => {
                                 </a>
                                 <a
                                     className="text-white font-bold bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 rounded-lg text-sm px-4 py-2 lg:px-5 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 cursor-pointer"
-                                    href="/play/"
+                                    href="/play/index.html"
                                 >
                                     Play now
                                 </a>
