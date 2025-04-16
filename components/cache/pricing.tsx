7254c87a4d0e4ab605a2abc7ed7a71aa26e0ccd2
pigeatgarlic <huyhoangdo0205@gmail.com>
2025-04-16 15:22:19 +0000

    update plan description text

--- components/cache/pricing.tsx
+++ components/cache/pricing.tsx
@@ -124,7 +124,7 @@ const Addon = {
             {value == 0 ? (
                 <span className="line-through">Giới hạn dung lượng</span>
             ) : (
-                <span>{value}GB credit dung lượng</span>
+                <span>{value} credit dung lượng</span>
             )}
         </li>
     )
@@ -138,8 +138,9 @@ const subcontents = [
         bonus: {
             time: 50,
             storage_limit: 200,
-            storage_credit: 100,
+            storage_credit: 150 * 15,
             no_waiting_line: false,
+            storage_credit: 200 * 30,
             multiple_cluster: false
         }
     },
@@ -150,7 +151,6 @@ const subcontents = [
         bonus: {
             time: 120,
             storage_limit: 400,
-            storage_credit: 200,
             no_waiting_line: false,
             multiple_cluster: false
         }
