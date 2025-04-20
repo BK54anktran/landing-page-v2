1e66768dbbed3e096fde61859cb3d148eafe76f2
epitchi <thienvanlea1@gmail.com>
2025-04-20 15:23:27 +0700

    fix footer ref

--- components/index.tsx
+++ components/index.tsx
@@ -369,7 +369,7 @@ export const CTA = () => {
                         Hoặc nhắn tin hỗ trợ để được trải nghiệm miễn phí
                     </p>
                     <a
-                        href="/play/index.html?app=null&ref=footerplay"
+                        href="/play/index.html?app=null&ref=landingpage_footerplay"
                         className="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900"
                     >
                         Sử dụng ngay
