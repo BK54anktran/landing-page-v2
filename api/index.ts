0322d9657ac942bc489bd2d96e029de8dcce22e7
epitchi <thienvanlea1@gmail.com>
2025-05-14 17:04:42 +0700

    change default server

--- api/index.ts
+++ api/index.ts
@@ -3,10 +3,10 @@ import PocketBase from 'pocketbase';
 export function getFrontendURL(): string {
     try {
         const address = localStorage.getItem('thinkmay_domain');
-        if (address == null) return 'https://play.2.thinkmay.net';
+        if (address == null) return 'https://saigon2.thinkmay.net';
         else return `https://${address}`;
     } catch {
-        return 'https://play.2.thinkmay.net';
+        return 'https://saigon2.thinkmay.net';
     }
 }
 export const POCKETBASE = () => new PocketBase(getFrontendURL());
